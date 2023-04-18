93366c06937ebeb124ce2a6bf6777d7c69d28b9a
Taher Jerbi <jerbi.taherr@gmail.com>
2023-04-18 01:50:51 +0100

    feat: return HTML

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{net::{TcpListener, TcpStream}, io::{BufReader, BufRead}};
+use std::{net::{TcpListener, TcpStream}, io::{BufReader, BufRead, Write}, fs};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -20,4 +20,13 @@ fn handle_connection(mut stream: TcpStream) {
         .collect();
 
     println!("Request: {:#?}", http_request);
+
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+
+    let response = 
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        
+    stream.write_all(response.as_bytes()).unwrap();
 }
